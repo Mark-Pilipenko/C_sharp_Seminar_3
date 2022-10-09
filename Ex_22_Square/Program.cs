7f7416ec0e6dccffbae703cce5b8bdcff086e22d
Mark <kalmark@mail.ru>
2022-10-09 23:08:59 +0300

    Add Ex_22_Square

---
+++ Ex_22_Square/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 22: Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+bool one = true;
+while (one)
+{
+    try
+    {
+        Console.Write("Введите натуральное число N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        if (N > 0)
+        {
+            Console.Write("Квадраты чисел от 1 до N: ");
+            for (int i = 1; i <= N; i++)
+            {
+                if (i == N) Console.Write(Math.Pow(i, 2) + ".");
+                else Console.Write(Math.Pow(i, 2) + ", ");
+            }
+            one = false;
+        }
+        else Console.WriteLine("Число не натуральное!");
+    }
+    catch
+    {
+        Console.WriteLine("Ошибка ввода!");
+    }
+}
