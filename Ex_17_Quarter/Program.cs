470f94e833e08c2c76f96e291a4b436c72d06f8f
Mark <kalmark@mail.ru>
2022-10-09 21:09:09 +0300

    Add Ex_17_Quarter

---
+++ Ex_17_Quarter/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > 0 && y > 0) Console.WriteLine("Point is in 1-st quarter.");
+else if (x < 0 && y > 0) Console.WriteLine("Point is in 2-nd quarter.");
+else if (x < 0 && y < 0) Console.WriteLine("Point is in 3-d quarter.");
+else if (x > 0 && y < 0) Console.WriteLine("Point is in 4-th quarter.");
